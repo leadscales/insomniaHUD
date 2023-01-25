f235b5b6ab49391274373ec3c34b7773c6893514
Lead <87102884+p3tr1ch0r@users.noreply.github.com>
2023-01-24 23:53:50 -0600

    Create run.bat
    Dev QOL

---
+++ dev/run.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+SET /a height=1080
+IF %1==43 (
+	SET /a width=1440
+)
+IF %1==169 (
+	SET /a width=1920
+)
+IF %1==1610 (
+	SET /a width=1728
+)
+TASKKILL /IM "hl2.exe" /FI "STATUS eq RUNNING"
+CD "C:\Program Files (x86)\Steam"
+steam -applaunch 440 -w %width% -h %height%
+ECHO Running TF2 with a width of %width% and a height of %height%
