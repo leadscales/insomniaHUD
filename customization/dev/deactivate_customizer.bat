379aaa1046d2dd8d99a2185492a5309ca697e51a
Lead <87102884+p3tr1ch0r@users.noreply.github.com>
2023-01-28 19:59:56 -0600

    Custom loading messages

--- customization/dev/deactivate_customizer.bat
+++ customization/dev/deactivate_customizer.bat
@@ -2,5 +2,9 @@
 (
 	CD tf\custom\insomniaHUD\customization\dev
 	REN customization_menu.res _customization_menu.res
+	CD ..\..\materials\console
+	REN startup_loading.vtf startup_loading_customizer.vtf
+	REN _startup_loading.vtf startup_loading.vtf
+	CD ..\..\customization\dev
 	restart_tf2.bat
 ) 1> nul
